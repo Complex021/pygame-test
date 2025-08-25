372c05a449d2a97c66010dd90129f244d8947036
Complex021 <flaaledimo@gmail.com>
2025-08-25 15:11:30 -0300

    add and render player

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import *
 def main():
     pygame.init
     black = pygame.Color('black')
     dt = 0
     clock = pygame.time.Clock()
+    PlayerCharacter = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )  
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,7 +16,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, black)
+        PlayerCharacter.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 100
+      
 if __name__ == "__main__":
     main()
